216bd8b7366c888e4d91420d049e47288f8de708
danielbonkowsky <danielbonkowsky@gmail.com>
2026-03-29 12:46:42 -0600

    Added TODOs with rules

--- main.py
+++ main.py
@@ -44,10 +44,15 @@ def main():
                 sum(assignments[(p, w, "B")] for p in schedule.names) == 0
             )
     
-    # One task per person
+    # Max of one task per person per week
     for p in schedule.names:
         for w in schedule.weeks:
             model.Add(sum(assignments[(p, w, t)] for t in schedule.tasks) <= 1)
+
+    # TODO: A person cannot be assigned A Call 2 weeks in a row
+    # TODO: Prefer > 2 weeks between A Call assignments
+    # TODO: A’ or B call can be assigned up to 2 weeks in a row
+    # TODO: C call can be assigned up to 2 weeks in a row; this is preferred but not required
 
     # Russ rules
     for w in schedule.weeks:
@@ -57,6 +62,9 @@ def main():
 
     model.Add(sum(assignments[("Russ", w, "A")] for w in schedule.weeks) == 2)
     # TODO: Russ can only be on A call when there is a fellow
+
+    # TODO: If someone has 0 instances of a task in the Assignments table, they cannot be assigned that task
+    # TODO: Persons can be given one more or one less task than assigned, but the overall number of tasks for a single person should not vary by more than +1 or -1 from the total of A+B+C assigned to that person
 
 
 if __name__ == "__main__":
