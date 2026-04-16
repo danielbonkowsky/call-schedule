b96c742038a3c1340e0c8b95c5006c2466a1c057
danielbonkowsky <danielbonkowsky@gmail.com>
2026-04-16 10:47:16 -0700

    Added more rules

--- main.py
+++ main.py
@@ -49,10 +49,43 @@ def main():
         for w in schedule.weeks:
             model.Add(sum(assignments[(p, w, t)] for t in schedule.tasks) <= 1)
 
-    # TODO: A person cannot be assigned A Call 2 weeks in a row
-    # TODO: Prefer > 2 weeks between A Call assignments
-    # TODO: A’ or B call can be assigned up to 2 weeks in a row
-    # TODO: C call can be assigned up to 2 weeks in a row; this is preferred but not required
+    # A person cannot be assigned A Call 2 weeks in a row
+    for p in schedule.names:
+        for i in range(len(schedule.weeks) - 1):
+            w1, w2 = schedule.weeks[i], schedule.weeks[i + 1]
+            model.Add(assignments[(p, w1, "A")] + assignments[(p, w2, "A")] <= 1)
+
+    # Prefer > 2 weeks between A Call assignments (soft constraint)
+    a_close_penalties = []
+    for p in schedule.names:
+        for i in range(len(schedule.weeks) - 2):
+            w1, w3 = schedule.weeks[i], schedule.weeks[i + 2]
+            close = model.NewBoolVar(f"a_close_{p}_{i}")
+            model.AddMinEquality(close, [assignments[(p, w1, "A")], assignments[(p, w3, "A")]])
+            a_close_penalties.append(close)
+
+    # A’ or B call can be assigned up to 2 weeks in a row
+    for p in schedule.names:
+        for i in range(len(schedule.weeks) - 2):
+            w1, w2, w3 = schedule.weeks[i], schedule.weeks[i + 1], schedule.weeks[i + 2]
+            model.Add(
+                assignments[(p, w1, "A'")] + assignments[(p, w2, "A'")] + assignments[(p, w3, "A'")] <= 2
+            )
+            model.Add(
+                assignments[(p, w1, "B")] + assignments[(p, w2, "B")] + assignments[(p, w3, "B")] <= 2
+            )
+
+    # C call can be assigned up to 2 weeks in a row (soft constraint -- preferred but not required)
+    c_streak_penalties = []
+    for p in schedule.names:
+        for i in range(len(schedule.weeks) - 2):
+            w1, w2, w3 = schedule.weeks[i], schedule.weeks[i + 1], schedule.weeks[i + 2]
+            streak = model.NewBoolVar(f"c_streak_{p}_{i}")
+            model.AddMinEquality(
+                streak,
+                [assignments[(p, w1, "C")], assignments[(p, w2, "C")], assignments[(p, w3, "C")]]
+            )
+            c_streak_penalties.append(streak)
 
     # Russ rules
     for w in schedule.weeks:
@@ -61,11 +94,41 @@ def main():
         model.Add(assignments[("Russ", w, "C")] == 0)
 
     model.Add(sum(assignments[("Russ", w, "A")] for w in schedule.weeks) == 2)
-    # TODO: Russ can only be on A call when there is a fellow
 
-    # TODO: If someone has 0 instances of a task in the Assignments table, they cannot be assigned that task
-    # TODO: Persons can be given one more or one less task than assigned, but the overall number of tasks for a single person should not vary by more than +1 or -1 from the total of A+B+C assigned to that person
+    # Russ can only be on A call when there is a fellow
+    for w in schedule.weeks:
+        if not schedule.week_has_fellow(w):
+            model.Add(assignments[("Russ", w, "A")] == 0)
 
+    # If someone has 0 instances of a task in the Assignments table, they cannot be assigned that task
+    for p in schedule.names:
+        for t in schedule.tasks:
+            if schedule.target_task_amount(p, t) == 0:
+                for w in schedule.weeks:
+                    model.Add(assignments[(p, w, t)] == 0)
+
+    # Total assignments per person should not vary by more than +/-1 from their target total
+    for p in schedule.names:
+        total_target = sum(schedule.target_task_amount(p, t) for t in schedule.tasks)
+        total_assigned = sum(assignments[(p, w, t)] for w in schedule.weeks for t in schedule.tasks)
+        model.Add(total_assigned >= total_target - 1)
+        model.Add(total_assigned <= total_target + 1)
+
+    model.Minimize(sum(a_close_penalties) + sum(c_streak_penalties))
+
+    solver = cp_model.CpSolver()
+    status = solver.Solve(model)
+
+    if status == cp_model.OPTIMAL:
+        print("Optimal solution found")
+    elif status == cp_model.FEASIBLE:
+        print("Feasible solution found (but not proven optimal)")
+    elif status == cp_model.INFEASIBLE:
+        print("No solution exists")
+    elif status == cp_model.MODEL_INVALID:
+        print("Model is invalid")
+    else:  # UNKNOWN
+        print("Solver stopped before finding a solution")
 
 if __name__ == "__main__":
     sys.exit(main())
